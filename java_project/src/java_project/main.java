6124f9e43dd5424c43b4ed08abd54165cbdfa3e4
sinaburokim8725 <sinaburokim8725@gmail.com>
2019-02-28 18:18:11 +0900

    initial project version

---
+++ java_project/src/java_project/main.java
@@ -0,0 +1,10 @@
+package java_project;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main Hello Java 안냥 ! ♥");
+	}
+
+}
